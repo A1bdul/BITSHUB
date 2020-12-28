6d419548fdab6b5c9b537c91569ce2a373dfae86
Your Name <you@example.com>
2020-12-28 20:49:13 +0100

    BITSHUB

--- Site/admin.py
+++ Site/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-
 from Site.models import Article, Comment, NewsLetter
 
-admin.site.register(Article)
 admin.site.register(Comment)
 admin.site.register(NewsLetter)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'Author')
+    search_fields = ('title',)
+    list_filter = ('title','Author')
+    list_display_links = ('title',)
+
+
+admin.site.register(Article, ArticleAdmin)
