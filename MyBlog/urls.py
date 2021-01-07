184748cfdac35a2b91cb0a392d0610ad42b265df
Your Name <you@example.com>
2021-01-07 02:08:29 +0100

    BITSHUB

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from filebrowser.sites import site
 urlpatterns = [
+    path('admin/filebrowser/', site.urls),
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', include('User.urls')),
     path('', include('Site.urls')),
