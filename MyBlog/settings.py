8c6b7ac04d04f5a55bd407338d8ef2affbed7597
Your Name <you@example.com>
2021-01-07 06:59:16 +0100

    BITSHUB

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -125,6 +125,16 @@ else:
             'PASSWORD': '',
         }
     }
+# [END db_setup]
+
+# Use a in-memory sqlite3 database when testing in CI systems
+if os.getenv('TRAMPOLINE_CI', None):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
